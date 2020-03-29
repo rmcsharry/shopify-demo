7e04314d7439faed195712d3464309669edd504d
Richard McSharry <rmcsharry@gmail.com>
2020-03-29 18:29:00 +0200

    Adds 2nd page - annotated layout

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "54e2fd72cab2135ce1ee";
+/******/ 	var hotCurrentHash = "bb107c25e022b9237c0b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
