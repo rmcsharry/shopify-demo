6583a25ac607f07a9791095bcc4d5d137c4aa252
Richard McSharry <rmcsharry@gmail.com>
2020-03-29 21:03:31 +0200

    Adds apollo client

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1585505352790");
+            injectCss(link, link.href.split("?")[0] + "?unix=1585508480841");
           });
       }
     }
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7eda26fce6fb470537b4";
+/******/ 	var hotCurrentHash = "cbc4433cceb24a0f42c1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
