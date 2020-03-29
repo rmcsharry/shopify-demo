c097bda9cd756f1aab32a55ea6817db1c777ac22
Richard McSharry <rmcsharry@gmail.com>
2020-03-29 18:24:20 +0200

    Adds EmptyState comp

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import { Page, Layout, TextStyle } from '@shopify/polaris';
+import { EmptyState, Page, Layout, TextStyle } from '@shopify/polaris';
+
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 const Index = () => (
   <Page>
@@ -6,6 +8,16 @@ const Index = () => (
       <TextStyle variation="positive">
         Richard's Sample app using React and Next.js
       </TextStyle>
+      <EmptyState
+        heading="Discount your products temporarily"
+        action={{
+          content: 'Select products',
+          onAction: () => console.log('clicked'),
+        }}
+        image={img}
+      >
+        <p>Select products to change their price temporarily.</p>
+      </EmptyState>
     </Layout>
   </Page>
 );
